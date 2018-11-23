991815fd9f6ea1c58034719b57ba1bc49a39f90b
marieroca <marieroca@172.17.47.208>
2018-11-23 15:39:33 +0100

    Màj avec les classes de départ :  - créa de la BD  - réception JMS  - interface

--- src/servicejuridique/ServiceJuridique.java
+++ src/servicejuridique/ServiceJuridique.java
@@ -5,6 +5,18 @@
  */
 package servicejuridique;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import servicejuridique.sources.PreConvention;
+
 /**
  *
  * @author hugo
@@ -14,8 +26,91 @@ public class ServiceJuridique {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws NamingException, JMSException {
+        //System.setProperty
+        System.setProperty("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
+        System.setProperty("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
+        System.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+        InitialContext context = new InitialContext();
+        
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "jms/__defaultConnectionFactory";
+        String destName = "ConventionEnCours";
+        Destination dest = null;
+        int count = 100;
+        Session session = null;
+        MessageConsumer receiver = null;
+        
+        //Toutes les connections sont gérées par le serveur
+        //A dégager 
+        try {
+            // create the JNDI initial context
+            context = new InitialContext();
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                    false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the receiver
+            //je veux recevoir toutes les conventions
+            receiver = session.createConsumer(dest);
+
+            // start the connection, to enable message receipt
+            connection.start();
+
+            for (int i = 0; i < count; ++i) {
+                Message message = receiver.receive();
+                if (message instanceof ObjectMessage) {
+                    //on récupère le message
+                    ObjectMessage flux = (ObjectMessage) message;
+                    //on récupère l'objet dans le message
+                    Object preconvention = flux.getObject();
+                    //on récupère le titre boursier dans l'objet
+                    if (preconvention instanceof PreConvention) {
+                        PreConvention convention = (PreConvention) preconvention;
+                        
+                        //Remplacer ci-dessou et afficher dans l'interface graphique
+                        System.out.println("Received: " + convention + " " + message.getStringProperty("date"));
+
+                    }
+
+                } else if (message != null) {
+                    System.out.println("Pas de préconvention reçue");
+                }
+            }
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
     }
     
 }
